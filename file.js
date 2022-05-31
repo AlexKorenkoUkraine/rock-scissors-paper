955bd0f9e31ef3c8897facca31b2458c78bffea3
AlexKorenko <lexa21shane@gmail.com>
2022-06-01 01:32:35 +0300

    added round function logic

--- file.js
+++ file.js
@@ -10,4 +10,36 @@ function computerPlay() {
         return 'Paper';
     }
 }
-alert(computerPlay());
+function round(){
+    let playerSelection = computerPlay();
+    let computerSelection = computerPlay();
+    if (playerSelection==='Rock'&&computerSelection ==='Scissors'){
+        return 'You win! Rock beats Scissors';
+    }
+    else if(playerSelection==='Scissors'&&computerSelection==='Paper'){
+        return 'You win! Scissors beats Paper';
+    }
+    else if(playerSelection==='Paper'&&computerSelection==='Rock'){
+        return 'You win! Paper beats Rock';
+    }
+    else if(playerSelection==='Rock'&&computerSelection==='Paper'){
+        return 'You lose! Paper beats Rock';
+    }
+    else if(playerSelection==='Scissors'&&computerSelection==='Rock'){
+        return 'You lose! Rock beats Scissors';
+    }
+    else if(playerSelection==='Paper'&&computerSelection==='Scissors'){
+        return 'You lose! Scissors beats Paper';
+    }
+    else if(playerSelection==='Rock'&&computerSelection==='Rock'){
+        return 'Draw game! Both thrown Rock';
+    }
+    else if(playerSelection==='Paper'&&computerSelection==='Paper'){
+        return 'Draw game! Both thrown Paper';
+    }
+    else if(playerSelection==='Scissors'&&computerSelection==='Scissors'){
+        return 'Draw game! Both thrown Scissors';
+    }
+    else return 'Error';
+}
+
