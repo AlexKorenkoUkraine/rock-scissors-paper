dd0b4a0cef69c4f75049f8467f372a1b9e3ee33e
AlexKorenko <lexa21shane@gmail.com>
2022-06-01 03:52:08 +0300

    finished version 1.0 only script

--- file.js
+++ file.js
@@ -3,44 +3,110 @@ function computerPlay() {
     if (a<=0.333333333333) {
         return 'rock';
     }
-    else if (a<=0.6666666666) {
+    else if (a<=0.666666666666) {
         return 'scissors';
     }
     else {
         return 'paper';
     }
 }
+let player = 0;
+let computer = 0;
+function checker(){
+    if (player==5) {
+        alert('Player WIN!');
+        return 'Player WIN!';
+    }
+    else if(computer==5){
+        alert('Computer WIN!');
+        return 'Computer WIN!';
+    }
+    else round();
+}
 function round(){
     let playerSelection = prompt('Make your Choose: Rock, Paper or Scissors?', '');
     let computerSelection = computerPlay();
-    if (playerSelection.toLowerCase()==='rock'&&computerSelection =='scissors'){
-        return 'You win! Rock beats Scissors';
+    if (playerSelection.toLowerCase()==='rock'&&computerSelection ==='scissors'){
+        player = player +1;
+        alert('You win! Rock beats Scissors');
+        checker();
     }
     else if(playerSelection.toLowerCase()==='scissors'&&computerSelection==='paper'){
-        return 'You win! Scissors beats Paper';
+        player = player +1;
+        alert('You win! Scissors beats Paper');
+        checker();
     }
     else if(playerSelection.toLowerCase()==='paper'&&computerSelection==='rock'){
-        return 'You win! Paper beats Rock';
+        player = player +1;
+        alert('You win! Paper beats Rock');
+        checker();
     }
     else if(playerSelection.toLowerCase()==='rock'&&computerSelection==='paper'){
-        return 'You lose! Paper beats Rock';
+        computer = computer +1;
+        alert('You lose! Paper beats Rock');
+        checker();
     }
     else if(playerSelection.toLowerCase()==='scissors'&&computerSelection==='rock'){
-        return 'You lose! Rock beats Scissors';
+        computer = computer +1;
+        alert('You lose! Rock beats Scissors');
+        checker();
     }
     else if(playerSelection.toLowerCase()==='paper'&&computerSelection==='scissors'){
-        return 'You lose! Scissors beats Paper';
+        computer = computer +1;
+        alert('You lose! Scissors beats Paper');
+        checker();
     }
     else if(playerSelection.toLowerCase()==='rock'&&computerSelection==='rock'){
-        return 'Draw game! Both thrown Rock';
+        alert('Draw game! Both thrown Rock');
+        checker();
     }
     else if(playerSelection.toLowerCase()==='paper'&&computerSelection==='paper'){
-        return 'Draw game! Both thrown Paper';
+        alert('Draw game! Both thrown Paper');
+        checker();
     }
     else if(playerSelection.toLowerCase()==='scissors'&&computerSelection==='scissors'){
-        return 'Draw game! Both thrown Scissors';
+        alert('Draw game! Both thrown Scissors');
+        checker();
     }
-    else return 'Error, please make your choose again!';
+    else {alert('Error, please make your choose again!');
+    checker();}
 }
-alert(round());
+round();
+
+/*function game(){
+    let player = 1;
+    let computer = 1;    
+    
+    if (player==5){
+        return 'Player is the Winner!';
+    }
+    else if (computer==5){
+        return 'Computer is the Winner!';}
+
+    else if (round()=='You win! Paper beats Rock'||round()=='You win! Rock beats Scissors'||round()=='You win! Scissors beats Paper'){
+        for (i=0;i<5;i++){
+        player = player * i;}
+        }
+    else if (round()=='You lose! Paper beats Rock'||round()=='You lose! Rock beats Scissors'||round()=='You lose! Scissors beats Paper'){
+        for (i=0;i<5;i++) {
+        computer = computer * i;}
+        }
+        
+        }*/
+/*function game(){
+    let player = 1;
+    let computer = 1;
+    for (i=1;i<=5;i++){
+        if (round()=='You win! Paper beats Rock'||round()=='You win! Rock beats Scissors'||round()=='You win! Scissors beats Paper'){
+            player = player * i;
+        }
+        else if (round()=='You lose! Paper beats Rock'||round()=='You lose! Rock beats Scissors'||round()=='You lose! Scissors beats Paper'){
+            computer = computer * i;
+        }}
+        if (player==5){
+            return 'Player is the Winner!';
+        }
+        else if (computer==5){
+            return 'Computer is the Winner!';
+        }*/
 
